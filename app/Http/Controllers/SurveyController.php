cbb7d63a5c8e58f5bff3ca371ae59c37a039046f
Jasper Guldentops <r0760673@student.thomasmore.be>
2021-02-01 14:00:06 +0100

    Changes to survey controller

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\MultiplechoiceItem;
 use App\MultiplechoiceQuestion;
 use App\OpenQuestion;
+use App\Rules\OpenQuestions;
 use App\Survey;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -75,24 +76,23 @@ class SurveyController extends Controller
     {
         $validator = Validator::make($request->all(),
             [
-                'user_id'                       => ['required', 'integer', 'exists:users,id'],
-                'name'                          => ['required', 'min:6', 'max:255', 'string', 'unique:surveys,name'],
-                'description'                   => ['required', 'min:6', 'max:255', 'string'],
-                'start_date'                    => ['required', 'date', 'after_or_equal:now'],
-                'end_date'                      => ['required', 'date', 'after_or_equal:start_date']
+                'user_id'                           => ['required', 'integer', 'exists:users,id'],
+                'name'                              => ['required', 'min:6', 'max:255', 'string', 'unique:surveys,name'],
+                'description'                       => ['max:255', 'string'],
+                'start_date'                        => ['required', 'date', 'after_or_equal:now'],
+                'end_date'                          => ['required', 'date', 'after_or_equal:start_date'],
             ],
             [
-                'required'                      => ':attribute moet ingevuld zijn',
-                'exists'                        => ':attribute bestaat niet binnen users',
-                'min'                           => ':attribute moet minstens 6 karakters lang zijn',
-                'max'                           => ':attribute mag maximum 255 karakters lang zijn',
-                'string'                        => ':attribute moet een string zijn',
-                'date'                          => ':attribute moet een geldige datum zijn',
-                'integer'                       => ':attribute moet een integer zijn',
-                'unique'                        => ':attribute is al in gebruik',
-                'start_date.after_or_equal'     => ':attribute mag niet in het verleden vallen',
-                'end_date.after_or_equal'       => ':attribute moet na de startdatum vallen'
-
+                'required'                          => ':attribute moet ingevuld zijn',
+                'exists'                            => ':attribute bestaat niet binnen users',
+                'min'                               => ':attribute moet minstens 6 karakters lang zijn',
+                'max'                               => ':attribute mag maximum 255 karakters lang zijn',
+                'string'                            => ':attribute moet een string zijn',
+                'date'                              => ':attribute moet een geldige datum zijn',
+                'integer'                           => ':attribute moet een integer zijn',
+                'unique'                            => ':attribute is al in gebruik',
+                'start_date.after_or_equal'         => ':attribute mag niet in het verleden vallen',
+                'end_date.after_or_equal'           => ':attribute moet na de startdatum vallen',
             ]);
         //On validation fail
         if ($validator->fails())
@@ -160,7 +160,7 @@ class SurveyController extends Controller
             [
                 'user_id'                       => ['required', 'integer', 'exists:users,id'],
                 'name'                          => ['required', 'min:6', 'max:255', 'string', 'unique:surveys,name,' .$survey->id],
-                'description'                   => ['required', 'min:6', 'max:255', 'string'],
+                'description'                   => ['max:255', 'string'],
                 'start_date'                    => ['required', 'date'],
                 'end_date'                      => ['required', 'date', 'after_or_equal:start_date']
             ],
