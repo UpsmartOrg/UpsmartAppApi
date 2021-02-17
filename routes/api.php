f8076104548e01122629dffdb8fc47de34f13b08
Jasper Guldentops <jasper.guldentops@gmail.com>
2021-02-17 13:15:49 +0100

    Moved routes into authenticated locations

--- routes/api.php
+++ routes/api.php
@@ -21,59 +21,11 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     // Authentication routes (public)
     Route::post('/login', 'Auth\AuthController@login')->name('login.api');
 
-    // Authenticated routes
-    Route::middleware('auth:sanctum')->group(function () {
-        //Logging out
-        Route::post('/logout', 'Auth\AuthController@logout')->name('logout.api');
-
-        //Routes met authentication komen uiteindelijk hier
-    });
-
-    Route::post('change-password', 'Auth\ResetPasswordController@resetPassword');
-
-    // Roles Routes
-
-    Route::get('roles', 'RoleController@index');
-    Route::get('roles/{role}', 'RoleController@show');
-
-    // {object} i.p.v {id} zet ID automatisch om naar een object. Indien object niet bestaat -> 404
-    // localhost:8000/api/users/1 -> returns user met ID 1, als hij niet bestaat returnt het 404
-
-    // Users Routes
-    Route::get('users', 'UserController@index');
-    Route::get('users/withroles', 'UserController@indexWithRoles');
-    Route::get('users/{user}', 'UserController@show');
-    Route::get('users/withroles/{user}', 'UserController@showWithRoles');
-
-    Route::post('users', 'UserController@store');
-    Route::post('users/withroles', 'UserController@storeWithRoles');
-    Route::put('users/{user}', 'UserController@update');
-    Route::put('users/withroles/{user}', 'UserController@updateWithRoles');
-    Route::delete('users/{user}', 'UserController@delete');
-
-    // UserRoles Routes
-    Route::get('userroles', 'UserRoleController@index');
-    Route::get('userroles/{userRole}', 'UserRoleController@show');
-    Route::post('userroles', 'UserRoleController@store');
-    Route::put('userroles/{userRole}', 'UserRoleController@update');
-    Route::delete('userroles/{userRole}', 'UserRoleController@delete');
-
-    // Information Routes
     Route::get('information', 'InformationController@index');
-    Route::get('information/withUser', 'InformationController@indexUser');
     Route::get('information/{information}', 'InformationController@show');
-    Route::post('information', 'InformationController@store');
-    Route::put('information/{information}', 'InformationController@update');
-    Route::delete('information/{information}', 'InformationController@delete');
 
-    // Surveys Routes
     Route::get('surveys', 'SurveyController@index');
-    Route::get('surveys/withuser', 'SurveyController@indexWithUser');
     Route::get('surveys/{survey}', 'SurveyController@show');
-    Route::get('surveys/complete/{survey}', 'SurveyController@showComplete');
-    Route::post('surveys/complete', 'SurveyController@storeComplete');
-    Route::put('surveys/complete/{survey}', 'SurveyController@updateComplete');
-    Route::delete('surveys/{survey}', 'SurveyController@delete');
 
     // OpenQuestions Routes
     Route::get('open_questions/from-survey/{surveyID}', 'OpenQuestionController@showFromSurvey');
@@ -87,36 +39,81 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::get('multi_items/from-question/{questionID}', 'MultiplechoiceItemController@showFromQuestion');
 
     // Answers Routes
-    Route::get('answers', 'AnswerController@index');
-    Route::get('answers/{answer}', 'AnswerController@show');
     Route::post('answers', 'AnswerController@store');
-    Route::delete('answers/{answer}', 'AnswerController@delete');
 
-    // Bin Routes
-    Route::get('bins', 'BinController@index');
-    Route::get('bins/all', 'BinController@indexAllBins');
-    Route::get('bins/test', 'BinController@test');
+    // Authenticated routes
+    Route::middleware('auth:sanctum')->group(function () {
+        //Logging out
+        Route::post('/logout', 'Auth\AuthController@logout')->name('logout.api');
 
-    // Zone Routes
-    Route::get('zones', 'ZoneController@index');
-    Route::get('zones/withbins', 'ZoneController@indexWithBins');
-    Route::get('zones/{zone}', 'ZoneController@show');
-    Route::get('zones/withbins/{zone}', 'ZoneController@showWithBins');
-    Route::post('zones', 'ZoneController@store');
-    Route::put('zones/{zone}', 'ZoneController@update');
-    Route::put('zones/bins/{zone}', 'ZoneController@updateZoneBins');
-    Route::delete('zones/{zone}', 'ZoneController@delete');
+        Route::get('users', 'UserController@index');
+        Route::get('users', 'UserController@index');
+        Route::get('users/withroles', 'UserController@indexWithRoles');
+        Route::get('users/{user}', 'UserController@show');
+        Route::get('users/withroles/{user}', 'UserController@showWithRoles');
+        Route::put('users/{user}', 'UserController@update');
 
-    // Bininfo routes
-    Route::get('bininfo', 'BinInfoController@index');
-    Route::get('bininfo/byzone/{zone}', 'BinInfoController@indexByZone');
-    Route::get('bininfo/nozone', 'BinInfoController@indexNoZone');
-    Route::get('bininfo/{binInfo}', 'BinInfoController@show');
-    Route::post('bininfo/update', 'BinInfoController@loadNewBins');
-    Route::put('bininfo/update/coords/{binInfo}', 'BinInfoController@updateBinCoordinates');
-    Route::put('bininfo/{binInfo}', 'BinInfoController@update');
-    Route::put('bininfo/zone/{binInfo}', 'BinInfoController@updateZone');
-    Route::delete('bininfo/{binInfo}', 'BinInfoController@delete');
+        Route::get('roles', 'RoleController@index');
+        Route::get('roles/{role}', 'RoleController@show');
+
+
+
+        Route::middleware('api.communication')->group(function () {
+            Route::get('information/withUser', 'InformationController@indexUser');
+            Route::post('information', 'InformationController@store');
+            Route::put('information/{information}', 'InformationController@update');
+            Route::delete('information/{information}', 'InformationController@delete');
+        });
+        Route::middleware('api.participation')->group(function () {
+            Route::get('surveys/withuser', 'SurveyController@indexWithUser');
+            Route::get('surveys/complete/{survey}', 'SurveyController@showComplete');
+            Route::post('surveys/complete', 'SurveyController@storeComplete');
+            Route::put('surveys/complete/{survey}', 'SurveyController@updateComplete');
+            Route::delete('surveys/{survey}', 'SurveyController@delete');
+
+            Route::get('answers', 'AnswerController@index');
+            Route::get('answers/{answer}', 'AnswerController@show');
+            Route::delete('answers/{answer}', 'AnswerController@delete');
+        });
+        Route::middleware('api.trash')->group(function () {
+            //bin routes for garbage collection
+            Route::get('bins', 'BinController@index');
+            //zone routes for garbage collection
+            Route::get('zones', 'ZoneController@index');
+            Route::get('zones/withbins', 'ZoneController@indexWithBins');
+            Route::get('zones/{zone}', 'ZoneController@show');
+            Route::get('zones/withbins/{zone}', 'ZoneController@showWithBins');
+            Route::post('zones', 'ZoneController@store');
+            Route::put('zones/{zone}', 'ZoneController@update');
+            Route::put('zones/bins/{zone}', 'ZoneController@updateZoneBins');
+            Route::delete('zones/{zone}', 'ZoneController@delete');
+            //bininfo routes for garbage collection
+            Route::get('bininfo', 'BinInfoController@index');
+            Route::get('bininfo/byzone/{zone}', 'BinInfoController@indexByZone');
+            Route::get('bininfo/nozone', 'BinInfoController@indexNoZone');
+            Route::get('bininfo/{binInfo}', 'BinInfoController@show');
+            Route::post('bininfo/update', 'BinInfoController@loadNewBins');
+            Route::put('bininfo/update/coords/{binInfo}', 'BinInfoController@updateBinCoordinates');
+            Route::put('bininfo/{binInfo}', 'BinInfoController@update');
+            Route::put('bininfo/zone/{binInfo}', 'BinInfoController@updateZone');
+            Route::delete('bininfo/{binInfo}', 'BinInfoController@delete');
+        });
+        Route::middleware('api.admin')->group(function () {
+            //User routes for admin
+            Route::post('users', 'UserController@store');
+            Route::post('users/withroles', 'UserController@storeWithRoles');
+            Route::put('users/withroles/{user}', 'UserController@updateWithRoles');
+            Route::delete('users/{user}', 'UserController@delete');
+            //Userrole routes for admin
+            Route::get('userroles', 'UserRoleController@index');
+            Route::get('userroles/{userRole}', 'UserRoleController@show');
+            Route::post('userroles', 'UserRoleController@store');
+            Route::put('userroles/{userRole}', 'UserRoleController@update');
+            Route::delete('userroles/{userRole}', 'UserRoleController@delete');
+        });
+    });
+
+    Route::post('change-password', 'Auth\ResetPasswordController@resetPassword');
 });
 
 
